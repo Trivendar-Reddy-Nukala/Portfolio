0881a505e4d61c5af482334a7ae3d11bbf67d70c
Trivendar-Reddy-Nukala <trivendar2005@gmail.com>
2024-06-26 07:58:04 +0530

    new changes

--- cert.js
+++ cert.js
@@ -1,24 +1,28 @@
 // cert.js
 
-// Main page script
-if (document.querySelector('.Cert_photos')) {
-    let imgs = document.getElementsByClassName("Cert_img");
-    Array.from(imgs).forEach(img => {
-        img.addEventListener("click", () => {
-            let img_link = img.src;
-            console.log(img_link);
-            localStorage.setItem('imgLink', img_link);
-            window.location = "show_img.html";
-        });
-    });
-}
+// Check if the current page should handle image clicks or display an image.
+if (window.innerWidth > 480) {
 
-// Display page script
-if (document.getElementById('show_img')) {
-    window.addEventListener('load', () => {
-        let img_link = localStorage.getItem('imgLink');
-        if (img_link) {
-            document.getElementById('show_img').src = img_link;
-        }
-    });
+    // Main page script
+    if (document.querySelector('.Cert_photos')) {
+        let imgs = document.getElementsByClassName("Cert_img");
+        Array.from(imgs).forEach(img => {
+            img.addEventListener("click", () => {
+                let img_link = img.src;
+                console.log(img_link);
+                localStorage.setItem('imgLink', img_link);
+                window.location = "show_img.html";
+            });
+        });
+    }
+
+    // Display page script
+    if (document.getElementById('show_img')) {
+        window.addEventListener('load', () => {
+            let img_link = localStorage.getItem('imgLink');
+            if (img_link) {
+                document.getElementById('show_img').src = img_link;
+            }
+        });
+    }
 }
